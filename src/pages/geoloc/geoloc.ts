c2ef264df44845f108e8ae8c785065ad4b084edc
Jean-Marc AUBERT <jmaubert1333@gmail.com>
2017-11-14 17:10:54 +0100

    ajout geoloc final

--- src/pages/geoloc/geoloc.ts
+++ src/pages/geoloc/geoloc.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
-  Generated class for the GeolocPage page.
-
-  See https://ionicframework.com/docs/components/#navigation for more info on
-  Ionic pages and navigation.
-
 
 @IonicPage()
 @Component({
