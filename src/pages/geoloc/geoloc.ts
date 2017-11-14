d7b502ee35ebc82e56794c0d5888c1589ad6eac6
Jean-Marc AUBERT <jmaubert1333@gmail.com>
2017-11-14 12:22:34 +0100

    ajout geoloc3

--- src/pages/geoloc/geoloc.ts
+++ src/pages/geoloc/geoloc.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the GeolocPage tabs.
  *
@@ -18,6 +18,19 @@ export class GeolocPage {
   geoRoot = 'GeoPage'
 
 
-  constructor(public navCtrl: NavController) {}
+  constructor(private geolocation: Geolocation) {}
 
 }
+
+this.geolocation.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+let watch = this.geolocation.watchPosition();
+watch.subscribe((data) => {
+ // data can be a set of coordinates, or an error (if an error occurred).
+ // data.coords.latitude
+ // data.coords.longitude
+});
